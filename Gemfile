8988274d638cf4d68997142348779bf324132a95
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-11-19 18:23:39 +0000

    added trainer class

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "https://rubygems.org"
+gem 'poke-api-v2'
 
 # Specify your gem's dependencies in pokemon_cli.gemspec
 gemspec
--- lib/trainer.rb
+++ lib/trainer.rb
@@ -1,3 +1,21 @@
-require 'poke-api-v2'
-bulbasaur = PokeApi.get(pokemon: 'bulbasaur')
-puts bulbasaur.methods
+# require 'poke-api-v2'
+# bulbasaur = PokeApi.get(pokemon: 'bulbasaur')
+# ivysaur = PokeApi.get(pokemon: 'ivysaur')
+# puts ivysaur.order
+
+class Trainer
+  
+  attr_accessor :name, :team
+  
+  def initialize(name)
+    @name = name
+    @team = []
+  end
+  
+  def catch_pokemon(pokemon)
+    puts "You throw a pokeball...You caught the wild pokemon!"
+    @team << pokemon
+  end
+  
+  
+end
