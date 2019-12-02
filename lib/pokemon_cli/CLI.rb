8b9866d7bc7674c40f6f5404769fe0bbbfd59563
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-12-02 20:27:11 +0000

    adding ability and type as attributes

--- lib/pokemon_cli/CLI.rb
+++ lib/pokemon_cli/CLI.rb
@@ -29,7 +29,8 @@ class CLI
     end
     
       wild_pokemon = PokeApi.get(pokemon: rand(806)+1)
-      pokemon = Pokemon.new(wild_pokemon.name,wild_pokemon.types,trainer_obj.name,wild_pokemon.height,wild_pokemon.weight)
+      binding.pry
+      pokemon = Pokemon.new(wild_pokemon.name,wild_pokemon.types[0].type.name,trainer_obj.name,wild_pokemon.abilities[0].ability.name)
       puts "You encountered a wild #{pokemon.name}!!"
       puts "Type 'pokeball' to attempt to capture the #{pokemon.name}."
       
